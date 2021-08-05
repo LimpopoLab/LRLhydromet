f1e2dcd7c856c87e228fdba806dc556c90434501
David Kahler <dmkahler@users.noreply.github.com>
2021-08-05 15:41:51 -0400

    fixed data dataframe calls to df

--- R/medike.R
+++ R/medike.R
@@ -9,20 +9,20 @@
 medike <- function() {
       df <- readr::read_csv(RCurl::getURL("https://data.usaid.gov/resource/chfb-yzu5.csv"))
       offset <- 2 
-      loc.dt <- as.numeric(lubridate::as_datetime(lubridate::ymd_hm(paste0(data$year, "-", data$mont, "-", data$dayn, "T", data$hour, ":", data$minu)))) 
-      utc.dt <- as.numeric(lubridate::as_datetime(lubridate::ymd_hm(paste0(data$year, "-", data$mont, "-", data$dayn, "T", data$hour, ":", data$minu)))) - (offset * 3600) 
-      loc.dy <- as.numeric(lubridate::as_date(lubridate::ymd(paste(data$year, "-", data$mont, "-", data$dayn))))
+      loc.dt <- as.numeric(lubridate::as_datetime(lubridate::ymd_hm(paste0(df$year, "-", df$mont, "-", df$dayn, "T", df$hour, ":", df$minu)))) 
+      utc.dt <- as.numeric(lubridate::as_datetime(lubridate::ymd_hm(paste0(df$year, "-", df$mont, "-", df$dayn, "T", df$hour, ":", df$minu)))) - (offset * 3600) 
+      loc.dy <- as.numeric(lubridate::as_date(lubridate::ymd(paste(df$year, "-", df$mont, "-", df$dayn))))
       met.dy <- loc.dy 
-      hydro.yr <- data$year 
-      hydro.mo <- data$mont 
-      for (i in 1:nrow(data)) {
-            if (data$mont[i] >= 8) { 
+      hydro.yr <- df$year 
+      hydro.mo <- df$mont 
+      for (i in 1:nrow(df)) {
+            if (df$mont[i] >= 8) { 
                   hydro.yr[i] <- hydro.yr[i] + 1 
                   hydro.mo[i] <- hydro.mo[i] - 7
             } else {
                   hydro.mo[i] <- hydro.mo[i] + 5
             }
-            if (data$hour[i] < 8) {
+            if (df$hour[i] < 8) {
                   met.dy[i] <- met.dy[i] - 1 
             }
       }
