5320d6cdae92dc1871193e7910ea179ca9b8c09a
David Kahler <dmkahler@users.noreply.github.com>
2021-08-08 21:28:54 -0400

    function to average wind direction, weighted averaged by speed.

--- R/wind.R
+++ R/wind.R
@@ -9,22 +9,29 @@
 #' are NAs in the input data array.
 #'
 #' @param t is angle array
+#' @param v is speed array
 #' @param dr is ("D")egrees or ("R")adians, default is degrees 
 #' @return averaged direction over all data
 #' @export
-wind <- function(t, dr = "D") {
+wind <- function(t, v, dr = "D") {
       if (dr == "D") {
+            final <- "D"
             direction <- 2 * pi * (t / 360)
             dr <- "R"
       } else {
+            final <- "R"
             direction <- t
       }
       if (dr == "R") {
-            s <- sin(direction)
+            v_bar <- mean(v)
+            s <- v * sin(direction)
             s_bar <- mean(s, na.rm = TRUE)
-            c <- cos(direction)
+            c <- v * cos(direction)
             c_bar <- mean(c, na.rm = TRUE)
-            
+            wind <- atan(s_bar/c_bar)
+            if (final == "D") {
+                  wind <- 360 * (wind / (2 * pi))
+            }
       } else {
             print("Please select D or R, in double quotes, for degrees or radians, respectively")
       }
